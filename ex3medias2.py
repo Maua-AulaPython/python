5d6c3148a062168caab13dcce07c80e877fabed4
Luciana Okuyama <LuOkuyama@Lucianas-MacBook-Air.local>
2015-03-20 15:50:12 -0300

    exercicio 3  Please enter the commit message for your changes. Lines starting

---
+++ ex3medias2.py
@@ -0,0 +1,21 @@
+#exercicio3: a lista termina quando eh digitado um numero negativo
+
+def media(a):
+	contador = 0
+	media = 0
+	while a > 0:
+		contador = contador + 1
+		media = media + a
+		print a
+		print media
+		print "digite uma nota"
+		a = int(raw_input())
+	else:
+		mediafinal = media/contador
+		print "a media final eh"
+		print mediafinal
+
+print "digite uma nota"
+a = int(raw_input())
+
+print media(a)
